ca74fa3834d0ac31674644f21d986dbd2409015a
Ahmad Irfan Mohammad Shukri <nerve2009@yahoo.com>
2020-03-20 00:16:08 +0800

    done

--- dj_project/settings.py
+++ dj_project/settings.py
@@ -129,7 +129,8 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static')
+    os.path.join(BASE_DIR, 'blog/static'),
+    os.path.join(BASE_DIR, 'users/static')
 ]
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
