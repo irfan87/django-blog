9b00ca7ef80418c72d3139147df087c3efb2c722
Ahmad Irfan Mohammad Shukri <nerve2009@yahoo.com>
2020-03-20 00:54:01 +0800

    add some changes

--- dj_project/settings.py
+++ dj_project/settings.py
@@ -21,12 +21,13 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'e0pmyf8p-in7f(u1f$gx8n&-xk5ygkva+!&b77ws5^27yjci^e'
+# SECRET_KEY = 'e0pmyf8p-in7f(u1f$gx8n&-xk5ygkva+!&b77ws5^27yjci^e'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['https://djangodummyblog.herokuapp.com']
 
 
 # Application definition
