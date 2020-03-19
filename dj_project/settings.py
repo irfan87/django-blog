7e10193fc36c4d0b22f2a18f1a27908027a2f67b
Ahmad Irfan Mohammad Shukri <nerve2009@yahoo.com>
2020-03-19 23:55:09 +0800

    correct spelling

--- dj_project/settings.py
+++ dj_project/settings.py
@@ -147,4 +147,4 @@ EMAIL_USE_TLS = True
 EMAIL_HOST_USER = os.environ.get('EMAIL_USER')
 EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_PASSWORD')
 
-django_heroku.settings(local())
+django_heroku.settings(locals())
